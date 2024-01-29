2cf57c607eb1c9900eb8aaf90c71d42e1a39574b
David Rios <david@davidrios.co>
2024-01-29 18:05:22 -0500

    fix: API Key disabled

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('SENDGRID_TEMPLATE', 'd-b8cf4302497045b390ff4f91fad41335');
-define('SENDGRID_LIST', 'f0112d42-2863-4ff4-8e28-dc7324fe1b78'); // Clientes Masterclas Detox la biologia del hambre
-define('SENDGRID_EMAIL_FROM', 'fernanda@fercasasbuenas.com');
+define('SENDGRID_TEMPLATE', 'xxxxx');
+define('SENDGRID_LIST', 'xxxxx');
+define('SENDGRID_EMAIL_FROM', 'xxxxx');
 define('SENDGRID_URL', 'https://api.sendgrid.com/v3');
-define('SENDGRID_APIKEY', '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');
+define('SENDGRID_APIKEY', 'xxxxx');
